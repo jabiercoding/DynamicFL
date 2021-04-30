40184b6607b75d591ede8d2857e098213e251534
jabiercoding <jabiercoding@gmail.com>
2021-04-30 16:49:05 +0200

    refactor

--- code/ArgoUMLSPLDynamicFL/src/dynamicfl/DynamicFL2BenchResults.java
+++ code/ArgoUMLSPLDynamicFL/src/dynamicfl/DynamicFL2BenchResults.java
@@ -18,10 +18,16 @@ public class DynamicFL2BenchResults {
 	/**
 	 * Compute results
 	 * 
-	 * @param pathToArgoUMLSPLBenchmark e.g., "C:/git/argouml-spl-benchmark/ArgoUMLSPLBenchmark"
+	 * @param pathToArgoUMLSPLBenchmark e.g.,
+	 *                                  "C:/git/argouml-spl-benchmark/ArgoUMLSPLBenchmark"
+	 * @return map of scenario, feature, (precision, recall, f1, classPrecision,
+	 *         classRecall, classF1)
 	 */
-	public static void compute(String pathToArgoUMLSPLBenchmark, Map<String,Map<String, List<Integer>>> featureClassAndLines, File output) {
-		
+	public static Map<String, Map<String, List<Double>>> compute(String pathToArgoUMLSPLBenchmark,
+			Map<String, Map<String, List<Integer>>> featureClassAndLines, File output, boolean onlyScenarioOriginal) {
+
+		Map<String, Map<String, List<Double>>> result = new LinkedHashMap<String, Map<String, List<Double>>>();
+
 		// Input path to the benchmark
 		File argoUMLSPLBenchmark = new File(pathToArgoUMLSPLBenchmark);
 
@@ -33,21 +39,18 @@ public class DynamicFL2BenchResults {
 				"scenarios/ScenarioOriginalVariant/variants/Original.config/src");
 		if (!originalArgoUMLsrc.exists()) {
 			System.out.println("Original scenario has to be created first");
-			return;
-		}
-
-		File outputScenarioFeatureMetrics = new File(output, "MetricsScenarioFeature.csv");
-		try {
-			FileUtils.writeFile(outputScenarioFeatureMetrics,
-					"Scenario;Feature;Precision;Recall;F1;ClassPrecision;ClassRecall;ClassF1\n");
-		} catch (Exception e1) {
-			e1.printStackTrace();
+			return result;
 		}
 
 		File scenariosFolder = new File(argoUMLSPLBenchmark, "scenarios");
 
 		for (File scenario : scenariosFolder.listFiles()) {
+
 			if (scenario.isDirectory()) {
+				if (onlyScenarioOriginal && !scenario.getName().equals("ScenarioOriginalVariant")) {
+					continue;
+				}
+
 				System.out.println("\nSCENARIO " + scenario.getName() + "\n\n");
 				// check if it it was created
 				File variants = new File(scenario, "variants");
@@ -55,6 +58,9 @@ public class DynamicFL2BenchResults {
 					System.out.println("Scenario's variants have to be created first");
 					continue;
 				}
+
+				Map<String, List<Double>> resultScenario = new LinkedHashMap<String, List<Double>>();
+				result.put(scenario.getName(), resultScenario);
 
 				// Scenario path
 				String scenarioPath = scenario.getAbsolutePath();
@@ -68,6 +74,9 @@ public class DynamicFL2BenchResults {
 
 				for (String currentFeature : fUtils.getFeatureIds()) {
 
+					List<Double> resultFeature = new ArrayList<Double>();
+					resultScenario.put(currentFeature, resultFeature);
+
 					System.out.println("\n" + currentFeature);
 
 					// Ground truth for the given feature
@@ -75,16 +84,17 @@ public class DynamicFL2BenchResults {
 							"groundTruth/" + currentFeature + ".txt");
 
 					Map<String, List<Integer>> classAndLines = featureClassAndLines.get(currentFeature);
-					
+
 					// feature is not considered
 					if (classAndLines == null) {
 						classAndLines = new HashMap<String, List<Integer>>();
 					}
-					
-					Map<String, List<Integer>> absPathAndLines = transformToAbsPathAndLines(originalArgoUMLsrc, classAndLines);
-					
-					List<String> results = LineTraces2BenchFormat.getResultsInBenchmarkFormat(absPathAndLines, currentFeature,
-							fUtils);
+
+					Map<String, List<Integer>> absPathAndLines = transformToAbsPathAndLines(originalArgoUMLsrc,
+							classAndLines);
+
+					List<String> results = LineTraces2BenchFormat.getResultsInBenchmarkFormat(absPathAndLines,
+							currentFeature, fUtils);
 
 					// Save to file
 
@@ -110,6 +120,9 @@ public class DynamicFL2BenchResults {
 					System.out.println("Precision: " + precision);
 					System.out.println("Recall: " + recall);
 					System.out.println("F1: " + f1);
+					resultFeature.add(precision);
+					resultFeature.add(recall);
+					resultFeature.add(f1);
 
 					System.out.println("\nUnofficial Class level metrics");
 					List<String> groundTruth2 = convertBenchTracesToClassLevel(groundTruth);
@@ -120,22 +133,49 @@ public class DynamicFL2BenchResults {
 					System.out.println("Precision: " + precision2);
 					System.out.println("Recall: " + recall2);
 					System.out.println("F1: " + f12);
-
-					try {
-						FileUtils.appendToFile(outputScenarioFeatureMetrics, scenario.getName() + ";" + currentFeature + ";"
-								+ precision + ";" + recall + ";" + f1 + ";" + precision2 + ";" + recall2 + ";" + f12);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+					resultFeature.add(precision2);
+					resultFeature.add(recall2);
+					resultFeature.add(f12);
 				}
 			}
 		}
+		resultsToFile(result, new File(output, "MetricsScenarioFeature.csv"));
+		return result;
 	}
 
-	
-	private static Map<String, List<Integer>> transformToAbsPathAndLines(File originalArgoUMLsrc, Map<String, List<Integer>> classAndLines) {
+	/**
+	 * Results to file
+	 * @param result
+	 * @param output
+	 */
+	public static void resultsToFile(Map<String, Map<String, List<Double>>> result, File output) {
+		try {
+			FileUtils.writeFile(output, "Scenario;Feature;Precision;Recall;F1;ClassPrecision;ClassRecall;ClassF1\n");
+			for (String scenario : result.keySet()) {
+				Map<String, List<Double>> scenarioFeatures = result.get(scenario);
+				for (String feature : scenarioFeatures.keySet()) {
+					List<Double> metrics = scenarioFeatures.get(feature);
+					FileUtils.appendToFile(output,
+							scenario + ";" + feature + ";" + metrics.get(0) + ";" + metrics.get(1) + ";"
+									+ metrics.get(2) + ";" + metrics.get(3) + ";" + metrics.get(4) + ";"
+									+ metrics.get(5));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Transform to absolute paths, keep the same lines
+	 * @param originalArgoUMLsrc
+	 * @param classAndLines
+	 * @return
+	 */
+	private static Map<String, List<Integer>> transformToAbsPathAndLines(File originalArgoUMLsrc,
+			Map<String, List<Integer>> classAndLines) {
 		Map<String, List<Integer>> result = new LinkedHashMap<String, List<Integer>>();
-		for(String key : classAndLines.keySet()) {
+		for (String key : classAndLines.keySet()) {
 			String absPath = key.replaceAll("\\.", "\\\\");
 			absPath = absPath + ".java";
 			File absFile = new File(originalArgoUMLsrc, absPath);
@@ -148,7 +188,6 @@ public class DynamicFL2BenchResults {
 		}
 		return result;
 	}
-
 
 	/**
 	 * Truncate traces to class level
@@ -167,6 +206,34 @@ public class DynamicFL2BenchResults {
 		converted.clear();
 		converted.addAll(s);
 		return converted;
+	}
+	
+	public static double getAvgPrecision(Map<String, Map<String, List<Double>>> result) {
+		return getAvg(result,0);
+	}
+	
+	public static double getAvgRecall(Map<String, Map<String, List<Double>>> result) {
+		return getAvg(result,1);
+	}
+	
+	public static double getAvgF1(Map<String, Map<String, List<Double>>> result) {
+		return getAvg(result,2);
+	}
+	
+	private static double getAvg(Map<String, Map<String, List<Double>>> result, int index) {
+		double avg = 0;
+		double numFeat = 0;
+		for (String scenario : result.keySet()) {
+			Map<String, List<Double>> scenarioFeatures = result.get(scenario);
+			for (String feature : scenarioFeatures.keySet()) {
+				List<Double> metrics = scenarioFeatures.get(feature);
+				if (!metrics.get(0).isNaN()) {
+					avg+=metrics.get(index);
+					numFeat++;
+				}
+			}
+		}
+		return avg / numFeat;
 	}
 
 }
--- code/ArgoUMLSPLDynamicFL/src/dynamicfl/gabrielaExec/MainSpectrum.java
+++ code/ArgoUMLSPLDynamicFL/src/dynamicfl/gabrielaExec/MainSpectrum.java
@@ -11,6 +11,7 @@ import dynamicfl.DynamicFL2BenchResults;
 import fk.stardust.localizer.IFaultLocalizer;
 import fk.stardust.localizer.Ranking;
 import fk.stardust.localizer.sbfl.Jaccard;
+import fk.stardust.localizer.sbfl.Ochiai;
 import fk.stardust.localizer.sbfl.Ochiai2;
 import fk.stardust.traces.INode;
 import fk.stardust.traces.ISpectra;
@@ -18,19 +19,22 @@ import fk.stardust.traces.ISpectra;
 public class MainSpectrum {
 
 	private static final double THRESHOLD_SBFL = 0.5;
-	
+
 	// Dataset from https://zenodo.org/record/4262529
 	final static String PATH_DATASET_EXECUTIONS = "C:/Users/106836/Downloads/Dataset/Dataset/ArgoUML/VariantsSourceCodeComparison/manual/variants";
 
 	final static String PATH_ARGOUMLSPL_BENCHMARK = "C:/git/argouml-spl-benchmark/ArgoUMLSPLBenchmark";
-	
+
+	final static boolean ONLY_ORIGINAL_SCENARIO = true;
+
 	public static void main(String[] args) {
-		
+
 		try {
-			
+
 			File output = new File("output/output_" + System.currentTimeMillis());
-			
-			Map<String, Map<String, List<Integer>>> featExec = GabrielaDatasetReader.getFeatExec(PATH_DATASET_EXECUTIONS);
+
+			Map<String, Map<String, List<Integer>>> featExec = GabrielaDatasetReader
+					.getFeatExec(PATH_DATASET_EXECUTIONS);
 
 			Map<String, Map<String, List<Integer>>> results = new LinkedHashMap<String, Map<String, List<Integer>>>();
 
@@ -40,7 +44,7 @@ public class MainSpectrum {
 				// prepare results
 				Map<String, List<Integer>> featResults = new LinkedHashMap<String, List<Integer>>();
 				results.put(feature, featResults);
-				
+
 				// Get the ranking from the SBFL technique
 				MySpectraProvider provider = new MySpectraProvider(feature, featExec);
 				ISpectra<String> spectra = provider.loadSpectra();
@@ -53,14 +57,14 @@ public class MainSpectrum {
 				// Add to the results if greater or equal to the threshold
 				System.out.println("\nFeature: " + feature + "\n");
 				Iterator<INode<String>> i = ranking.iterator();
-				while(i.hasNext()) {
+				while (i.hasNext()) {
 					INode<String> node = i.next();
 					double suspiciousness = ranking.getSuspiciousness(node);
 					if (suspiciousness >= THRESHOLD_SBFL) {
 						System.out.println(node.getIdentifier() + " " + suspiciousness);
 						String className = node.getIdentifier().split(";")[0];
 						Integer line = Integer.parseInt(node.getIdentifier().split(";")[1]);
-						
+
 						List<Integer> lines = featResults.get(className);
 						if (lines == null) {
 							lines = new ArrayList<Integer>();
@@ -70,14 +74,38 @@ public class MainSpectrum {
 					}
 				}
 			}
-			
+
 			// Compute results
-			DynamicFL2BenchResults.compute(PATH_ARGOUMLSPL_BENCHMARK, results, output);
+			// scenario, feature, (precision, recall, f1, classPrecision, classRecall,
+			// classF1)
+			Map<String, Map<String, List<Double>>> result = DynamicFL2BenchResults.compute(PATH_ARGOUMLSPL_BENCHMARK,
+					results, output, ONLY_ORIGINAL_SCENARIO);
+
+			System.out.println("\n6 Diagram features");
+			double avgPrecision = DynamicFL2BenchResults.getAvgPrecision(result);
+			double avgRecall = DynamicFL2BenchResults.getAvgRecall(result);
+			double avgF1 = DynamicFL2BenchResults.getAvgF1(result);
+			System.out.println("Avg. Precision:\t" + avgPrecision);
+			System.out.println("Avg. Recall:\t" + avgRecall);
+			System.out.println("Avg. F1:\t" + avgF1);
+			
+			System.out.println("\nResults from SPLC 2020");
+			System.out.println("2020 Precision:\t0.068333333");
+			System.out.println("2020 Recall:\t0.318333333");
+			System.out.println("2020 F1:\t0.105");
+
+			// Results from 2020 solution
+//			ActivityDiagram	0.05	0.24	0.08
+//			CollaborationDiagram	0.04	0.19	0.06
+//			DeploymentDiagram	0.04	0.45	0.07
+//			SequenceDiagram	0.12	0.25	0.16
+//			StateDiagram	0.08	0.31	0.13
+//			UseCaseDiagram 	0.08	0.47	0.13
+//			AVG:	0.068333333	0.318333333	0.105
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 
 }
